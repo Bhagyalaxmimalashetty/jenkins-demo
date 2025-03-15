2ea763afc95cd51dda2903f71afccd6d11ef6026
Bhagyalaxmi Malashetty <bhagyamalashetty@gmail.com>
2025-03-15 17:45:17 +0530

    jenkinsdemo/src/main/java/com/sbi/jenkinsdemo/JenkinsdemoApplication.java

--- jenkinsdemo/src/test/java/com/sbi/jenkinsdemo/JenkinsdemoApplicationTests.java
+++ jenkinsdemo/src/test/java/com/sbi/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -1,7 +1,11 @@
 package com.sbi.jenkinsdemo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JenkinsdemoApplicationTests {
